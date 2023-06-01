4dcf388f7c48802c7b6023484785088ce4e6b76e
mamertmg <marquezgarcia.a@gmail.com>
2023-06-01 06:47:27 +0200

    first commit

---
+++ backend/controllers/user.Controller.js
@@ -0,0 +1,45 @@
+import asyncHandler from 'express-async-handler'
+
+// @desc    Auth user & get token
+// @route   POST /api/users/auth
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+    res.status(200).json({message:'Auth User'})
+});
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({message:'Register User'})
+});;
+
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/logout
+// @access  Public
+const logoutUser = (req, res) => {
+    res.status(200).json({ message: 'Logged out successfully' });
+}
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'User Profile' });
+});;
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Update User Profile' });
+});
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+};
+
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,8 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+
+const protect = asyncHandler(async (req, res, next) => {
+
+});
+
+export { protect };
