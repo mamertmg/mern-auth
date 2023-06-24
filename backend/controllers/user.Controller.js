c3262fde68a7c0b0904dd5be586be2abc8fcd01a
mamertmg <marquezgarcia.a@gmail.com>
2023-06-24 14:09:56 +0200

    added register end point

--- backend/controllers/user.Controller.js
+++ backend/controllers/user.Controller.js
@@ -11,8 +11,34 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message:'Register User'})
-});;
+    const { name, email, password } = req.body;
+  
+    const userExists = await User.findOne({ email });
+  
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+    }
+  
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+  
+    if (user) {
+      generateToken(res, user._id);
+  
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid user data');
+    }
+});
 
 // @desc    Logout user / clear cookie
 // @route   POST /api/users/logout
--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,12 @@ import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
+connectDB();
+
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-connectDB();
 
 app.use('/api/users', userRoutes);
 
